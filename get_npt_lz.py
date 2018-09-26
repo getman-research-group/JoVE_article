c7b441e9450f6c920f3c4252837012641e8e8bec
Cameron Bodenschatz <cbodens@g.clemson.edu>
2018-09-26 16:54:42 -0400

    Add files via upload

---
+++ get_npt_lz.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+log_filename = sys.argv[1]
+
+def read_log(log_filename):
+    log_file = open(log_filename,'r')
+    section = ''
+    subsection = ''
+    
+    nvt_file = open('nvt_data.txt','w+')
+    nve_file = open('nve_data.txt','w+')
+    npt_file = open('npt_data.txt','w+')
+    
+    for line in log_file:
+        if line.startswith('------------ beginning equilibration (const Vol)------------'):
+            section = 'nvt'
+            continue
+        elif line.startswith('------------ check energy conservation (only nve)------------'):
+            section = 'nve'
+            continue
+        elif line.startswith('------------ beginning npt (use drag)------------'):
+            section = 'npt'
+            continue
+        elif line.startswith('Loop'):
+            section = ''
+            subsection = ''
+            continue
+
+        if section == 'nvt':
+            if line.startswith('Step'):
+                subsection = 'nvt_data'
+                line_data = line.split()
+                nvt_temp_index = line_data.index('Temp')
+                step = line_data[0]
+                temp = line_data[nvt_temp_index]
+                nvt_file.write('%-10s %-10s \n' % (step, temp))
+                continue
+        elif section == 'nve':
+            if line.startswith('Step'):
+                subsection = 'nve_data'
+                line_data = line.split()
+                nve_enrg_index = line_data.index('TotEng')
+                step = line_data[0]
+                enrg = line_data[nve_enrg_index]
+                nve_file.write('%-10s %-10s \n' % (step, enrg))
+                continue
+        elif section == 'npt':
+            if line.startswith('Step'):
+                subsection = 'npt_data'
+                line_data = line.split()
+                npt_lz_index = line_data.index('Lz')
+                step = line_data[0]
+                lz = line_data[npt_lz_index]
+                npt_file.write('%-10s %-10s \n' % (step, lz))
+                continue
+
+        if subsection == 'nvt_data':
+            line_data = line.split()
+            step = line_data[0]
+            temp = line_data[nvt_temp_index]
+            nvt_file.write('%-10s %-10s \n' % (step, temp))
+        elif subsection == 'nve_data':
+            line_data = line.split()
+            step = line_data[0]
+            enrg = line_data[nve_enrg_index]
+            nve_file.write('%-10s %-10s \n' % (step, enrg))
+        elif subsection == 'npt_data':
+            line_data = line.split()
+            step = line_data[0]
+            lz   = line_data[npt_lz_index]
+            npt_file.write('%-10s %-10s \n' % (step, lz))
+
+    log_file.close()
+    nvt_file.close()
+    nve_file.close()
+    npt_file.close()
+
+read_log(log_filename)
+
+plt.switch_backend('agg')
+
+x = []
+y = []
+with open('nvt_data.txt') as f:
+    header = f.readline()
+    lines = f.readlines()
+    x = [int(line.split()[0]) for line in lines]
+    y = [float(line.split()[1]) for line in lines]
+fig = plt.figure()
+plt.plot(x, y)
+plt.title('NVT Temp vs. Step')
+plt.xlabel('Time Step')
+plt.ylabel('Temperature [K]')
+plt.savefig('nvt_plot.png')
+plt.close(fig)
+
+x = []
+y = []
+with open('nve_data.txt') as f:
+    header = f.readline()
+    lines = f.readlines()
+    x = [int(line.split()[0]) for line in lines]
+    y = [float(line.split()[1]) for line in lines]
+fig = plt.figure()
+plt.plot(x, y)
+plt.title('NVE Energy vs. Step')
+plt.xlabel('Time Step')
+plt.ylabel('Energy [eV]')
+plt.savefig('nve_plot.png')
+plt.close(fig)
+
+x = []
+y = []
+with open('npt_data.txt') as f:
+    header = f.readline()
+    lines = f.readlines()
+    x = [int(line.split()[0]) for line in lines]
+    y = [float(line.split()[1]) for line in lines]
+fig = plt.figure()
+plt.plot(x, y)
+plt.title('NPT Lz vs. Step')
+plt.xlabel('Time Step')
+plt.ylabel('Length of z-axis [A]')
+plt.savefig('npt_plot.png')
+plt.close(fig)
+
+with open('avg_lz.txt','w+') as f:
+    f.write('The average Lz is: %.16f' % np.mean(np.array(y[2001:])))
+
+
---
+++ lmps_bond_angle.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import sys
+
+# inPOSCAR = '/Users/cbodensc/Desktop/h5o2_box.POSCAR'
+# out_data = '/Users/cbodensc/Desktop/h5o2_box.bonds_angles_info.txt'
+
+# inPOSCAR = sys.argv[1]
+inPOSCAR = 'POSCAR'
+out_data = inPOSCAR + '.bond_angle_info.txt'
+
+def read_poscar(inPOSCAR):
+    file_poscar = open(inPOSCAR, 'r')
+    section = ''
+    title = []
+    multiplier = []
+    cell_vecs = []
+    coords = []
+    water_coords = []
+    
+    line_count = 1
+    atom_count = 1
+    for line in file_poscar:
+        if line_count == 1:
+            section = 'title'
+            title = line.rstrip('\n')
+        if line_count == 2:
+            section = 'multiplier'
+            multiplier = line.strip('\n').split()
+#            x_vec = file_poscar.next().strip('\n').split()
+#            y_vec = file_poscar.next().strip('\n').split()
+#            z_vec = file_poscar.next().strip('\n').split()
+            x_vec = next(file_poscar).strip('\n').split()
+            y_vec = next(file_poscar).strip('\n').split()
+            z_vec = next(file_poscar).strip('\n').split()
+            cell_vecs = [x_vec, y_vec, z_vec]
+            line_count = 5
+        if line_count == 6:
+            elements = line.strip('\n').split()
+        if line_count == 7:
+            atoms = line.strip('\n').strip()
+        if line_count == 8:
+            if line.lower().startswith('s'):
+                sel_dyn = 'Selective Dynamics'
+            else:
+                line_count += 1
+        if line_count == 9:
+            section = 'coord type'
+            if line.lower().startswith('c'):
+                coord_type = 'Cartesian'
+            elif line.lower().startswith('d'):
+                coord_type = 'Direct'
+            else:
+                sys.exit('ERROR: Incorrect coordinate type! Please change to Cartesian or Direct!')
+        if line_count == 10:
+            section = 'coords'
+        line_count += 1
+
+
+        if section == 'coords':
+            coord = [atom_count] + line.strip('\n').split()
+            if coord[-1] == '0':
+                water_coords.append(coord)
+            coords.append(coord)
+            atom_count += 1
+    
+    file_poscar.close()
+    
+    return water_coords
+
+def get_water_bonds_angles(water_coords):
+    bonds_list = []
+    angles_list = []
+    
+    if len(water_coords)%3 == 0:
+        num_waters = len(water_coords)/3
+    else:
+        sys.exit('ERROR: The number of Oxygens and Hydrogens does not sum to a multiple of 3!')
+
+    print(str(int(num_waters)) + ' water molecules were identified.')
+
+    o_coords = water_coords[:int(num_waters)]
+    h_coords = water_coords[int(num_waters):]
+    if len(h_coords) != 2*len(o_coords):
+        sys.exit('ERROR: There is an inconsistency between the number of Oxygens and Hydrogens!')
+        
+    for o_coord in o_coords:
+        for h_coord in h_coords:
+            if o_coord[4] == h_coord[4]:
+                bonds_list.append([o_coord[0], h_coord[0]])
+                
+    print(str(len(bonds_list)) + ' bonds were identified.')
+ 
+    for bond1 in bonds_list:
+        for bond2 in bonds_list:
+            if bond1[0] == bond2[0] and bond1[1] != bond2[1] and [bond2[1], bond1[0], bond1[1]] not in angles_list:
+                angles_list.append([bond1[1], bond1[0], bond2[1]])
+                
+    print(str(len(angles_list)) + ' angles were identified.')
+
+    with open(out_data, 'w') as f:
+        f.write(' Bonds \n')
+        f.write(' \n')
+        bond_counter = 1
+        for bond in bonds_list:
+            f.write(str(bond_counter) + ' 1 ' + str(bond[0]) + ' ' + str(bond[1]) + '\n')
+            bond_counter += 1
+        f.write(' \n')
+        f.write(' Angles \n')
+        f.write(' \n')
+        angle_counter = 1
+        for angle in angles_list:
+            f.write(str(angle_counter) + ' 1 ' + str(angle[0]) + ' ' + str(angle[1]) + ' ' + str(angle[2]) + '\n')
+            angle_counter += 1
+    
+water_coords = read_poscar(inPOSCAR)
+get_water_bonds_angles(water_coords)
